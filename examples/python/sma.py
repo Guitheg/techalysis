755ed01c5d715539a5637992f7aadea4cc912fcd
Guitheg <gthgobin@gmail.com>
2025-04-14 22:24:25 +0200

    Update SMA example in python

--- examples/python/sma.py
+++ examples/python/sma.py
@@ -1,25 +1,19 @@
 import numpy as np
 import pandas as pd
 import technicalysis as ta
-import talib
+import matplotlib.pyplot as plt
 
-def np_sma(data: np.ndarray, window_size: int) -> np.ndarray:
-    if data.size < window_size:
-        return np.array([])  # Return an empty array if window size is larger than data.
-    # Create a uniform window and compute the moving average using convolution.
-    window = np.ones(window_size) / window_size
-    return np.convolve(data, window, mode='valid')
 
 if __name__ == "__main__":
     close = np.random.random(100)
-    close[55] = np.nan
-    print(close)
-    close_df = pd.Series(close)
-    out = ta.sma(close, 3)
-    out2 = np_sma(close, 3)
-    # out_df = ta.sma(close_df, 10)
-    # out = talib.SMA(close)
-    # out_df = talib.SMA(close_df)
+    plt.plot(close)
 
-    print(f"Out: Numpy:{out}\nPandas:{out2}")
+    out = ta.sma(close, 20)
+    print(f"Input: {close}")
+    print(f"Output1: {out}")
+    plt.plot(out)
+
+    # show
+    plt.title('Simple Moving Average')
+    plt.show()
 
