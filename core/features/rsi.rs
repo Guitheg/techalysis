a55656921c98c21275ecd0f02494bddde3217117
Guitheg <gthgobin@gmail.com>
2025-05-21 01:15:56 +0200

    Optimize rsi performance : better than C now

--- core/features/rsi.rs
+++ core/features/rsi.rs
@@ -1,6 +1,7 @@
 use crate::errors::TechnicalysisError;
 use std::f64;
 
+#[inline(always)]
 fn calculate_rsi(avg_gain: f64, avg_loss: f64) -> f64 {
     if avg_loss == 0.0 {
         return 100.0;
@@ -11,6 +12,7 @@ fn calculate_rsi(avg_gain: f64, avg_loss: f64) -> f64 {
 
 pub fn rsi(data_array: &[f64], window_size: usize) -> Result<Vec<f64>, TechnicalysisError> {
     let size = data_array.len();
+    let period = window_size as f64;
     if window_size == 0 || window_size + 1 > size {
         return Err(TechnicalysisError::InsufficientData);
     }
@@ -19,36 +21,38 @@ pub fn rsi(data_array: &[f64], window_size: usize) -> Result<Vec<f64>, Technical
         return Ok(data_array.to_vec());
     }
 
+    let k = 1.0 / window_size as f64;
+    let one_minus_k = 1.0 - k;
     let mut result = vec![f64::NAN; size];
 
     let mut avg_gain: f64 = 0.0;
     let mut avg_loss: f64 = 0.0;
     for i in 1..=window_size {
-        if data_array[i].is_nan() {
+        let delta = data_array[i] - data_array[i - 1];
+        if delta.is_nan() {
             return Err(TechnicalysisError::UnexpectedNan);
         }
-        let delta = data_array[i] - data_array[i - 1];
         if delta > 0.0 {
             avg_gain += delta;
         } else {
             avg_loss -= delta;
         }
     }
-    avg_gain /= window_size as f64;
-    avg_loss /= window_size as f64;
+    avg_gain /= period;
+    avg_loss /= period;
     result[window_size] = calculate_rsi(avg_gain, avg_loss);
 
     for i in (window_size + 1)..size {
-        if data_array[i].is_nan() {
+        let delta = data_array[i] - data_array[i - 1];
+        if delta.is_nan() {
             return Err(TechnicalysisError::UnexpectedNan);
         }
-        let delta = data_array[i] - data_array[i - 1];
         if delta > 0.0 {
-            avg_gain = (avg_gain * (window_size as f64 - 1.0) + delta) / window_size as f64;
-            avg_loss = (avg_loss * (window_size as f64 - 1.0)) / window_size as f64;
+            avg_gain = avg_gain * one_minus_k + delta * k;
+            avg_loss *= one_minus_k;
         } else {
-            avg_gain = (avg_gain * (window_size as f64 - 1.0)) / window_size as f64;
-            avg_loss = (avg_loss * (window_size as f64 - 1.0) - delta) / window_size as f64;
+            avg_gain *= one_minus_k;
+            avg_loss = avg_loss * one_minus_k - delta * k;
         }
         result[i] = calculate_rsi(avg_gain, avg_loss);
     }
