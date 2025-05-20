62c4018e29af729e6289c4dc09711f20b8f7979c
Guitheg <gthgobin@gmail.com>
2025-05-21 00:26:52 +0200

    remove dirty import from test_rsi

--- tests/rust/tests_rsi.rs
+++ tests/rust/tests_rsi.rs
@@ -1,6 +1,5 @@
 use crate::oracle_test;
 use crate::rust::tests_helper::{assert::assert_vec_close, oracle::read_fixture};
-use proptest::{collection::vec, prelude::*};
-use technicalysis::{errors::TechnicalysisError, features::rsi::rsi};
+use technicalysis::features::rsi::rsi;
 
 oracle_test!(rsi, |x: &[f64]| rsi(x, 14));
