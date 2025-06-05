e0edfbdc2f067d7da28deae9697359793ac71659
Guitheg <gthgobin@gmail.com>
2025-06-05 18:11:46 +0200

    Move fuzz testing folder and update CI configuration

--- tests/fuzz/run_all.sh
+++ tests/fuzz/run_all.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
-cd "$(dirname "$0")"
 
-
-mapfile -t targets < <(cargo fuzz list)
+FUZZ_DIR=tests/fuzz
 
 perform_fuzzing=1
 failures=()
 runs=5000000
 max_len=4096
 
+mapfile -t targets < <(cargo fuzz list --fuzz-dir $FUZZ_DIR)
+
+if (( ${#targets[@]} == 0 )); then
+  echo "❌ No fuzz targets found. Exiting."
+  exit 0
+  else
+  echo "Found ${#targets[@]} fuzz targets:"
+  for t in "${targets[@]}"; do
+    echo " - $t"
+  done
+fi
+
 for t in "${targets[@]}"; do
   echo "=== running $t ==="
-  if ! cargo +nightly fuzz run "$t" -s none -- -runs="$runs" -max_len="$max_len" ; then
+  if ! cargo +nightly fuzz run $t --fuzz-dir $FUZZ_DIR -s none -- -runs="$runs" -max_len="$max_len" ; then
     echo "❌  $t failed"
     failures+=("$t")
     perform_fuzzing=0
@@ -28,7 +38,7 @@ if ((${#failures[@]})); then
   exit 2
 else
   if (( perform_fuzzing )); then
-    echo "❌ Something went wrong :("
+    echo "❌ Something went wrong"
   else
     echo "✅ All fuzz targets passed 🎉"
   fi
